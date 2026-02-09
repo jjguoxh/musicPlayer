d7c28b1f2d2011a1904a94cfdcd97087f3e8c459
jjguoxh <jjguoxh@gmail.com>
2026-02-09 21:14:42 +0800

    Initial Commit

---
+++ musicPlayer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  musicPlayer
+//
+//  Created by jjguoxh@gmail.com on 2026/2/9.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ musicPlayer/musicPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  musicPlayerApp.swift
+//  musicPlayer
+//
+//  Created by jjguoxh@gmail.com on 2026/2/9.
+//
+
+import SwiftUI
+
+@main
+struct musicPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
